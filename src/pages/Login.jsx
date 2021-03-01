f047a143bc94f794a0fc8e608e92ab2c432d1e9e
Michelle Gude <michellecgude@gmail.com>
2021-02-28 22:41:50 -0500

    built out basic signup, login react in components, usestate, etc.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useState } from "react";
+
+import ".././styles/register.scss";
+
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 export default function Login() {
-    return (
-        <div>
-            
-        </div>
-    )
+  // sets initial state for username and password to an empty string
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const logValues = () => {
+    console.log(username);
+    console.log(password);
+  };
+
+  const handleUsername = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePassword = (event) => {
+    setPassword(event.target.value);
+  };
+
+  return (
+    <div className="test">
+      <Form>
+        {/*  this is where you'll pass the function/props from the app to signup  */}
+        <Form.Group controlId="registerForm">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="username"
+            placeholder="Enter your username."
+            name="username"
+            value={username}
+            onChange={handleUsername}
+          />
+          <Form.Text className="text-muted">
+            Welcome to the club, social butterfly.
+          </Form.Text>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter your Password."
+            name="password"
+            value={password}
+            onChange={handlePassword}
+          />
+          <Form.Text className="text-muted">
+            Keep your account secure!
+          </Form.Text>
+        </Form.Group>
+        <Button variant="dark" type="button" onClick={logValues}>
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -7,32 +7,77 @@ import Button from "react-bootstrap/Button";
 
 export default function Register() {
   // sets initial state for username and password to an empty string
-  [username, setUsername] = useState("")[(password, setPassword)] = useState(
-    ""
-  );
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleChange = (event) => {
-    return setUsername(event.target.value);
+  // helps me to check whether the form is working as intended
+  const logValues = () => {
+    console.log(name);
+    console.log(email);
+    console.log(username);
+    console.log(password);
+  };
+
+  const handleName = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleEmail = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleUsername = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePassword = (event) => {
+    setPassword(event.target.value);
   };
 
   return (
     <div className="test">
       <Form>
-        <Form.Group controlId="formUsername">
+        {/*  this is where you'll pass the function/props from the app to signup  */}
+        <Form.Group controlId="registerForm">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="name"
+            placeholder="Enter your name."
+            name="name"
+            value={name}
+            onChange={handleName}
+          />
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter your email."
+            name="email"
+            value={email}
+            onChange={handleEmail}
+          />
           <Form.Label>Username</Form.Label>
-          <Form.Control type="username" placeholder="Enter your username." />
+          <Form.Control
+            type="username"
+            placeholder="Enter your username."
+            name="username"
+            value={username}
+            onChange={handleUsername}
+          />
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter your Password."
+            name="password"
+            value={password}
+            onChange={handlePassword}
+          />
           <Form.Text className="text-muted">
             Welcome to the club, social butterfly.
           </Form.Text>
         </Form.Group>
-        <Form.Group controlId="formPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Enter your Password." />
-          <Form.Text className="text-muted">
-            Keep your account secure!
-          </Form.Text>
-        </Form.Group>
-        <Button variant="dark" type="submit">
+        <Button variant="dark" type="button" onClick={logValues}>
           Submit
         </Button>
       </Form>
