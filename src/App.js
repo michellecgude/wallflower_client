b25abd98524c46ea0479f3723fa208aeabb6a77b
Michelle Gude <michellecgude@gmail.com>
2021-03-01 13:28:22 -0600

    Cleaned up register component, auth.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
 
 import Homepage from "./pages/Homepage";
 import Register from "./pages/Register";
@@ -9,76 +9,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App(props) {
-  const [form, setForm] = "";
-  const [authenticated, setAuth] = localStorage.getItem("token")
-    ? "true"
-    : "false";
-  const [username, setUsername] = "";
-
-  useEffect(() => {
-    if (authenticated) {
-      fetch("https://wallflower-serverside.herokuapp.com/", {
-        headers: {
-          Authorization: `JWT ${localStorage.getItem("token")}`,
-        },
-      })
-        .then((res) => res.json())
-        .then((json) => {
-          setUsername(json.username);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }
-  }, []);
-
-  handleLogin = (event, data) => {
-    event.preventDefault();
-    fetch("https://wallflower-serverside.herokuapp.com/token-auth/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
-      .then((res) => res.json())
-      .then((json) => {
-        localStorage.setItem("token", json.token);
-        authenticated = true;
-        form = "";
-        username = json.user.username;
-      });
-  };
-
-  handleSignup = (event, data) => {
-    event.preventDefault();
-    fetch("http://localhost:8000/core/users/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
-      .then((res) => res.json())
-      .then((json) => {
-        localStorage.setItem("token", json.token);
-        authenticated = true;
-        form = "";
-        username = json.username;
-      });
-  };
-
-  handleLogout = () => {
-    localStorage.removeItem("token");
-    this.setState({ logged_in: false, username: "" });
-  };
-
-  display_form = (form) => {
-    this.setState({
-      displayed_form: form,
-    });
-  };
-
   return (
     <div className="App">
       <Homepage />
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -6,39 +6,29 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
 export default function Register() {
+  const initialFormState = {
+    name: "",
+    email: "",
+    username: "",
+    password: "",
+  };
+  const [formState, setFormState] = useState(initialFormState);
 
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-  // helps me to check whether the form is working as intended
-  const logValues = () => {
-    console.log(name);
-    console.log(email);
-    console.log(username);
-    console.log(password);
+  const handleChange = (event) => {
+    console.log(event.target.value);
+    setFormState({ ...formState, [event.target.id]: event.target.value });
   };
 
-  const handleName = (event) => {
-    setName(event.target.value);
-  };
-
-  const handleEmail = (event) => {
-    setEmail(event.target.value);
-  };
-
-  const handleUsername = (event) => {
-    setUsername(event.target.value);
-  };
-
-  const handlePassword = (event) => {
-    setPassword(event.target.value);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setFormState(formState);
+    // is this saving?
+    console.log(formState);
   };
 
   return (
     <div className="test">
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <h1>Register Here.</h1>
         {/*  this is where you'll pass the function/props from the app to signup  */}
         <Form.Group controlId="registerForm">
@@ -47,38 +37,38 @@ export default function Register() {
             type="name"
             placeholder="Enter your name."
             name="name"
-            value={name}
-            onChange={handleName}
+            defaultValue={formState.name}
+            onChange={handleChange}
           />
           <Form.Label>Email</Form.Label>
           <Form.Control
             type="email"
             placeholder="Enter your email."
             name="email"
-            value={email}
-            onChange={handleEmail}
+            defaultValue={formState.email}
+            onChange={handleChange}
           />
           <Form.Label>Username</Form.Label>
           <Form.Control
             type="username"
             placeholder="Enter your username."
             name="username"
-            value={username}
-            onChange={handleUsername}
+            defaultValue={formState.username}
+            onChange={handleChange}
           />
           <Form.Label>Password</Form.Label>
           <Form.Control
             type="password"
             placeholder="Enter your Password."
             name="password"
-            value={password}
-            onChange={handlePassword}
+            defaultValue={formState.password}
+            onChange={handleChange}
           />
           <Form.Text className="text-muted">
             Welcome to the club, social butterfly.
           </Form.Text>
         </Form.Group>
-        <Button variant="dark" type="button" onClick={logValues}>
+        <Button variant="dark" type="submit">
           Submit
         </Button>
       </Form>
