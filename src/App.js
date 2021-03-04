5202140d87e80abfb1f31015eaf4ea359f501952
Michelle Gude <michellecgude@gmail.com>
2021-03-04 02:17:02 -0600

    context to handle user auth globally, created values in app.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 // react imports
 import React, { useState, useEffect } from "react";
-import { Route, Switch } from "react-router-dom";
+import { useHistory, Redirect, Route, Switch } from "react-router-dom";
 
-// component imports
-import PrivateRoute from "./components/PrivateRoute";
-import LandingPage from "./components/LandingPage";
+// user component imports
+import Context from "./components/user_auth/Context";
 import UserRegister from "./components/user_auth/UserRegister";
 import UserLogin from "./components/user_auth/UserLogin";
-import MoodPrompt from "./components/MoodPrompt";
+
+// page component imports
+import PrivateRoute from "./components/PrivateRoute";
+import LandingPage from "./components/LandingPage";
+
+// dashboard component imports
 import Dashboard from "./components/dashboard/Dashboard";
+import MoodPrompt from "./components/MoodPrompt";
 import MoodBoard from "./components/dashboard/MoodBoard";
 import HabitBoard from "./components/dashboard/HabitBoard.jsx";
 import MeditationBoard from "./components/dashboard/MeditationBoard";
@@ -18,21 +23,44 @@ import UpliftingBoard from "./components/dashboard/UpliftingBoard";
 import "./App.css";
 
 export default function App() {
+  // global contextual user state for authentication
+  const [isAuthed, setAuth] = useState(false);
+
+  // checks for user authentication, sets auth depending upon email & token validation
+  const checkAuth = () => {
+    if (localStorage.email && localStorage.token) setAuth(true);
+  };
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
   return (
     <div className="App">
-      <Route exact path="/" component={LandingPage} />
-      <Route exact path="/register" component={UserRegister} />
-      <Route exact path="/login" component={UserLogin} />
-      <Route exact path="/howareyou" component={MoodPrompt} />
-      <Route exact path="/dashboard" component={Dashboard} />
-      <Route exact path="/dashboard/moods" component={MoodBoard} />
-      <Route exact path="/dashboard/habits" component={HabitBoard} />
-      <Route exact path="/dashboard/meditations" component={MeditationBoard} />
-      <Route
-        exact
-        path="/dashboard/upliftingcontent"
-        component={UpliftingBoard}
-      />
+      <Context.Provider
+        globalAuthValues={{
+          isAuthed,
+          setAuth,
+        }}
+      >
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/register" component={UserRegister} />
+        <Route exact path="/login" component={UserLogin} />
+        <Route exact path="/howareyou" component={MoodPrompt} />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/dashboard/moods" component={MoodBoard} />
+        <Route exact path="/dashboard/habits" component={HabitBoard} />
+        <Route
+          exact
+          path="/dashboard/meditations"
+          component={MeditationBoard}
+        />
+        <Route
+          exact
+          path="/dashboard/upliftingcontent"
+          component={UpliftingBoard}
+        />
+      </Context.Provider>
 
       {/* <Switch>
         <PrivateRoute exact path="/dashboard" component={Dashboard} />
