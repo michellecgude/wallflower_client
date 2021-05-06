6a345bfe822c48a795e0043120de1ac89b02f4c0
Michelle Gude <michellecgude@gmail.com>
2021-05-05 21:24:28 -0500

    added style files and app to commit.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from "./logo.svg";
+// REACT IMPORTS
+import React from "react";
+import { Route } from "react-router-dom";
+
+// STYLE IMPORTS
 import "./styles/App.scss";
+
+// COMPONENT IMPORTS
+import Register from "./components/user_auth/Register.jsx";
+import Login from "./components/user_auth/Login.jsx";
 
 function App() {
   return (
     <div className="App">
-      <h1>Test, test</h1>
+      <Route exact path="/register" component={Register} />
+      <Route exact path="/login" component={Login} />
     </div>
   );
 }
